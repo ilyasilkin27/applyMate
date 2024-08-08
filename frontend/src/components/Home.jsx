15d032c77d1ece8b2f308bd22f00d7e87d411e41
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-08-08 15:39:24 +0300

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,14 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = () => {
     return (
         <div className="container mt-5">
-            <h1>Список вакансий</h1>
-            <ul className="list-group">
-                {vacancies.map(vacancy => (
-                    <li key={vacancy.id} className="list-group-item">
-                        {vacancy.name}
-                    </li>
-                ))}
-            </ul>
+            <h1>Дом</h1>
         </div>
     );
 };
