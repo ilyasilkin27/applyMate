6b8330a4910786f3cc3a976b357a33cefcef2f0f
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-08-08 17:22:24 +0300

    fix api routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const Login = () => {
     const handleLogin = async () => {
         try {
-            const response = await axios.get('/api/authorize');
+            const response = await axios.get('https://applymatebackend-lem56lxuf-ilyas-projects-973327cb.vercel.app/api/authorize');
             const authorizationUrl = response.data.url;
             window.location.href = authorizationUrl;
         } catch (error) {
