9bd8b26c5d61196c9b8ee75e03f9c68565903f88
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-08-29 10:14:16 +0300

    update ResumeList.jsx

--- frontend/src/components/ResumeList.jsx
+++ frontend/src/components/ResumeList.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
 const ResumeList = ({ resumes, onSelect }) => {
+  const [selectedResumeId, setSelectedResumeId] = useState(null);
+
+  const handleSelect = (id) => {
+    setSelectedResumeId(id);
+    onSelect(id);
+  };
+
   return (
     <ListGroup>
       {resumes.map((resume) => (
         <ListGroup.Item
           key={resume.id}
-          onClick={() => onSelect(resume.id)}
-          style={{ cursor: "pointer" }}
+          onClick={() => handleSelect(resume.id)}
+          style={{
+            cursor: "pointer",
+            backgroundColor: selectedResumeId === resume.id ? "#0d6efd" : "",
+          }}
         >
-          <h5>{resume.title}</h5>
-          <p>
+          <h5
+            style={{
+              color: selectedResumeId === resume.id ? "white" : "",
+            }}
+          >
+            {resume.title}
+          </h5>
+          <p
+            style={{
+              color: selectedResumeId === resume.id ? "white" : "",
+            }}
+          >
             {resume.first_name} {resume.middle_name} {resume.last_name}
           </p>
         </ListGroup.Item>
