96943cedeb5984ed27fb34f81751919f2053532a
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-08-30 10:28:13 +0300

    update resumeController.js

--- backend/src/controllers/resumeController.js
+++ backend/src/controllers/resumeController.js
@@ -1,4 +1,31 @@
 import axios from "axios";
+import { USER_AGENT } from "../utils.js";
+
+export const getResumes = async (req, res) => {
+  const accessToken = req.cookies.access_token;
+  
+  if (!accessToken) {
+    return res.status(401).json({ error: "No access token provided" });
+  }
+
+  try {
+    const response = await axios.get(`https://api.hh.ru/resumes/mine`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "HH-User-Agent": USER_AGENT,
+      },
+      params: {
+        locale: "RU",
+        host: "hh.ru",
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Failed to fetch resumes", error);
+    res.status(500).json({ error: "Failed to fetch resumes" });
+  }
+};
 
 export const getSimilarVacancies = async (req, res) => {
   const { resumeId } = req.params;
